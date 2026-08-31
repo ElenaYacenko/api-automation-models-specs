bdae49f1e4dceac5dcceeb0910c4f6beaeb9b96e
ElenaYacenko <149763977+ElenaYacenko@users.noreply.github.com>
2026-08-31 17:01:28 +0300

    Add ClubsTests

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -23,7 +23,7 @@ public class TestData {
     public static final String invalidTelegramLink = "invalid-url";
     public static final String notFoundError = "No Club matches the given query.";
     public static final String unauthorizedError = "Authentication credentials were not provided.";
-    public static final String fieldRequiredError = "This field may not be blank.";    public static final String negativeYearError = "This field must be positive.";
+    public static final String negativeYearError = "This field must be positive.";
     public static final int nonExistentClubId = 999999;
 
     private static final Faker faker = new Faker();
